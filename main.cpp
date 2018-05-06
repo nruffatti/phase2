53854796b72e4189dadd59040c14dbf12582f333
brokebrandon <norcalnbk@gmail.com>
2018-05-05 19:34:38 -0500

    made some progress on main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@
  * Created on April 12, 2018, 9:45 AM
  */
 
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <algorithm>
-#include <vector>
 #include <queue>
+#include <sstream>
+#include <string>
+#include <vector>
 
 #include "part1.h"
 #include "customer.h"
@@ -22,17 +23,34 @@
 
 using namespace std;
 
+void printInvalid() {
+	cout << "Not valid input. Try again!" << endl;
+
+	//fixed infinite loop error for invalid inputs
+	cin.clear();
+	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int s_to_i(string str) {
+	istringstream buffer(str);
+	int num;
+	buffer >> num;
+
+	return num;
+}
+
 int main(int argc, char** argv) {
     // variables that will be used in multiple places or multiple times
     Customer * newCustomer;
     Order * newOrder;
     Transaction * newTransaction;
-    
+
     //data storage
     vector<Customer *> customerList;
     queue<string> rainbowQueue;
     vector<Order *> orderList;
     vector<Transaction *> transactionList;
+	vector<float> prices;
 
     vector<int> foundList; // to store the index of found records with duplicate name
     int choice;
@@ -49,7 +67,7 @@ int main(int argc, char** argv) {
     //    string s;
     //    cout << "Parse Data? (y/n)" << endl;
     //    cin >> s;
-    //    
+    //
     //    //user edits bad data
     //    if (s.compare("y") == 0) {
     //        data = getData(data);
@@ -65,14 +83,14 @@ int main(int argc, char** argv) {
     //        data = fix_bad(data);
     //        save(data, "fixed_data");
     //    } else { //uses last saved edited badData file
-    //        
+    //
     //        //data = getData(data);
     //        //data = rm_spaces(data, 4);
     //        //data = rm_spaces(data, 3);
     //        //data = rm_nonNum(data, 5);
     //        //data = fix_bad(data);
     //        //save(data, "fixed_data");
-    //        
+    //
     //        data = getCustomerData("customers",7);
     //    }
     //    cout << "\n\n\n\n\n";
@@ -82,19 +100,19 @@ int main(int argc, char** argv) {
         newCustomer = new Customer(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4], data[i][5], data[i][6]);
         customerList.push_back(newCustomer);
     }
-    
+
     // translate the data into order objects and push into orderList vector
     for (int i = 0; i < order.size(); i++) {
-        newOrder = new Order(order[i][0], order[i][1], order[i][2], order[i][3]);
+        newOrder = new Order(s_to_i(order[i][0]), order[i][1], s_to_i(order[i][2]), s_to_i(order[i][3]));
         orderList.push_back(newOrder);
     }
-    
+
     // translate the data into transaction objects and push into transactionList vector
     for (int i = 0; i < transaction.size(); i++) {
-        newTransaction = new Transaction(transaction[i][0], transaction[i][1]);
+        newTransaction = new Transaction(s_to_i(transaction[i][0]), s_to_i(transaction[i][1]));
         transactionList.push_back(newTransaction);
     }
-    
+
     //push rainbow data to queue
     for (int i = 0; i < rainbow.size(); i++) {
         rainbowQueue.push(rainbow[i][0]);
@@ -102,9 +120,9 @@ int main(int argc, char** argv) {
     // to keep the track of # of existing records
     tSize = transactionList.size();
     oSize = orderList.size();
-    cSize = customerList.size(); 
-    
-    
+    cSize = customerList.size();
+
+
     // vector is used to store the options that the user can choose
     vector<string> options;
     options.push_back("(1) Add a customer");
@@ -115,10 +133,18 @@ int main(int argc, char** argv) {
     options.push_back("(6) Exit");
 
 
+	// Vector used to store tribble prices
+	prices.push_back(9.50);
+	prices.push_back(16.15);
+	prices.push_back(25.88);
+	prices.push_back(28.15);
+	prices.push_back(30.00);
+
+
     // to store the user input
     string ID, fname, lname, street, city, state, zip;
-    string orderID, transactionID, date, quantity, amountPaid;
-    
+    string orderID, transactionID, date, quantity, amountPaid, date;
+
     while (!exit) {
 
         cout << "\n\nWhat would you like to do?\n";
@@ -140,8 +166,14 @@ int main(int argc, char** argv) {
          */
 
         switch (choice) {
-            case 1:
+            case 1: // User is prompted to add new customer @Author Brandon Youngquist
                 cout << "\nNew Customer\n============" << endl;
+				// cout << "\nWhat would you like to do?";
+				// cout << "\n(1) Sell to Customer";
+				// cout << "\n(2) Return to Main Menu";
+				// cout << "\n\nEnter an above number to continue: ";
+
+				cin >> choice;
 
                 ID = generateID(customerList);
 
@@ -168,7 +200,7 @@ int main(int argc, char** argv) {
                 customerList.push_back(newCustomer);
                 break;
 
-            case 2:
+            case 2: // User is prompted to look up a customer by last name
                 cout << "\nEnter last name to search: ";
                 cin >> lname;
                 foundList = searchLastName(customerList, lname);
@@ -197,7 +229,8 @@ int main(int argc, char** argv) {
                 } else
                     cout << "No match was found. Try again!" << endl;
                 break;
-            case 3:
+
+            case 3: // User is prompted to look up a customer by ID
                 cout << "\nEnter ID number to search: ";
                 cin >> ID;
                 foundList = searchID(customerList, ID);
@@ -210,16 +243,60 @@ int main(int argc, char** argv) {
                 } else
                     cout << "No Match was found. Try again!" << endl;
                 break;
-            case 4:
-                //customer sale
+
+            case 4: // Customer sale prompt @Author Brandon Youngquist
+				cout << "Customer Sale Menu\n===================";
+				cout << "\nWhat would you like to do?";
+				cout << "\n(1) Sell to Customer";
+				cout << "\n(2) Return to Main Menu";
+				cout << "\n\nEnter an above number to continue: ";
+
+				cin >> choice;
+
+				switch(choice) {
+					case 1: // Sell to customer
+						cout << "Please enter customer ID: ";
+						cin >> ID;
+
+						cout << "Please enter quantity (Limit 5): ";
+						cin >> quantity;
+						while(s_to_i(quantity) > 5 || s_to_i(quantity) < 0) {
+							cout << "Please enter a valid number: ";
+							cin >> quantity;
+						}
+
+						orderID = orderList.size() + 1;
+
+						// time_t rawtime;
+						//   struct tm * timeinfo;
+						//   char buffer [80];
+						//
+						//   time (&rawtime);
+						//   timeinfo = localtime (&rawtime);
+						//
+						//   strftime (buffer,80,"Now it's %I:%M%p.",timeinfo);
+
+						newTransaction = new Transaction(s_to_i(ID), s_to_i(quantity));
+
+						break;
+
+					case 2: // Return to main menu
+						// Do nothing
+						break;
+
+					default: // Invalid input
+						printInvalid();
+						break;
+				}
                 break;
-            case 5:
+
+            case 5: // User prompted to add customer to rainbow tribble queue
                 cout << "\nWhat would you like to do?\n";
                 cout << "(1) Add Customer to Queue\n";
                 cout << "(2) Sell to Customer on Queue\n";
                 cout << "\nEnter an above number to continue: ";
                 cin >> choice;
-                
+
                 //---------------------submenu----------------------
                 switch (choice) {
                     case 1:
@@ -241,15 +318,12 @@ int main(int argc, char** argv) {
                         rainbowQueue.pop();
                         break;
                     default:
-                        cout << "Not valid input. Try again!" << endl;
-
-                        //fixed infinite loop error for invalid inputs
-                        cin.clear();
-                        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        printInvalid();
                         break;
                 }
                 break;
-            case 6:
+
+            case 6: // Exit
                 updateRecordFile(customerList, cSize);
                 //need to check update order & transaction still
                 updateOrders(orderList, oSize);
@@ -259,11 +333,7 @@ int main(int argc, char** argv) {
                 exit = true;
                 break;
             default:
-                cout << "Not valid input. Try again!" << endl;
-
-                //fixed infinite loop error for invalid inputs
-                cin.clear();
-                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				printInvalid();
                 break;
         }
     }
--- transaction.h
+++ transaction.h
@@ -1,8 +1,6 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
-#include<string>
-
 using namespace std;
 
 class Transaction {
