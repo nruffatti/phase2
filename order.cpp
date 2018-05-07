19fbbeca11c636ff453efcfdd3a4a3ec4472fd0c
Hai Le <35050699+kurodatsubasa@users.noreply.github.com>
2018-05-07 17:49:47 -0500

    Rewrote to_string method
    Rewrote to_string method
    since it behaved weird when printing

--- order.cpp
+++ order.cpp
@@ -1,41 +1,44 @@
 #include "order.h"
+#include <sstream>
 
 Order::Order() {
-	this->orderID = 0;
-	this->date = "";
-	this->quantity = 0;
-	this->amountPaid = 0;
+    this->orderID = 0;
+    this->date = "";
+    this->quantity = 0;
+    this->amountPaid = 0;
 }
 
-Order::Order(int orderID, string date, int quantity, int amountPaid) {
-	this->orderID = orderID;
-	this->date = date;
-	this->quantity = quantity;
-	this->amountPaid = amountPaid;
+Order::Order(int orderID, string date, int quantity, float amountPaid) {
+    this->orderID = orderID;
+    this->date = date;
+    this->quantity = quantity;
+    this->amountPaid = amountPaid;
 }
 
 int Order::getOrderID() {
-	return orderID;
+    return orderID;
 }
 
 string Order::getDate() {
-	return date;
+    return date;
 }
 
 int Order::getQuantity() {
-	return quantity;
+    return quantity;
 }
 
-int Order::getAmountPaid() {
-	return amountPaid;
+float Order::getAmountPaid() {
+    return amountPaid;
 }
 
 string Order::to_string() {
-	string line;
-	line += "Order ID: " + orderID;
-	line += "\nDate Placed: " + date;
-	line += "\nQuantity Ordered: " + quantity;
-	line += "\nAmount Paid: " + amountPaid;
+    stringstream stream;
+    string line;
+    stream << "Order ID: " << orderID;
+    stream << "\nDate Placed: " << date;
+    stream << "\nQuantity Ordered: " << quantity;
+    stream << "\nAmount Paid: " << amountPaid;
 
-	return line;
+    line = stream.str();
+    return line;
 }
